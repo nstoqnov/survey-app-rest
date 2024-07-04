865408ceac2023142da4e64b6ac0fc87ee7ace5b
nikolaystoyanov <stoqnovn@yahoo.com>
2024-07-04 17:50:59 +0300

    init commit

---
+++ src/main/java/com/example/SurveyREST/SurveyRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SurveyREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SurveyREST/SurveyRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SurveyREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
